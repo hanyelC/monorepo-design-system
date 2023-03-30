6df16bd80870f301150a756ce02ca9bb689de540
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-30 09:29:42 -0300

    create Checkbox component

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,16 @@
+import { CheckboxContainer, CheckboxIndicator } from './styles'
+import { ComponentProps } from '../../types'
+
+import { Check } from 'phosphor-react'
+
+export type CheckboxProps = ComponentProps<typeof CheckboxContainer>
+
+export function Checkbox({ ...props }: CheckboxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
