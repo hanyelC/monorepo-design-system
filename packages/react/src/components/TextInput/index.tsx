e5fe9d6cdcef7b1aa52659c2f265bc1c5348f3db
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-30 09:00:55 -0300

    create TextInput component

---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps } from '../../types'
+import { Input, Prefix, TextInputContainer } from './styles'
+
+export type TextInputProps = {
+  prefix?: string
+} & ComponentProps<typeof Input>
+
+export function TextInput({ prefix, ...props }: TextInputProps) {
+  return (
+    <TextInputContainer>
+      {prefix ? <Prefix>{prefix}</Prefix> : null}
+      <Input {...props} />
+    </TextInputContainer>
+  )
+}
