7a9c54703228f7c94d542f6829180aa50381ce43
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-03-30 09:55:21 -0300

    create MultiStep component

---
+++ packages/react/src/components/MultiStep/index.tsx
@@ -0,0 +1,22 @@
+import { ComponentProps } from '../../types'
+import { Label, MultiStepContainer, Step, Steps } from './styles'
+
+export type MultiStepProps = {
+  size: number
+  currentStep?: number
+} & ComponentProps<typeof MultiStepContainer>
+
+export function MultiStep({ size, currentStep = 1 }: MultiStepProps) {
+  return (
+    <MultiStepContainer>
+      <Label>
+        Passo {currentStep} de {size}
+      </Label>
+      <Steps css={{ '--steps-size': size }}>
+        {Array.from({ length: size }, (_, i) => i + 1).map((step) => (
+          <Step key={step} active={currentStep >= step} />
+        ))}
+      </Steps>
+    </MultiStepContainer>
+  )
+}
